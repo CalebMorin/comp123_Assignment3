ea73803f0ba9eb3c9298887d6f2f1ac7d2e9a264
CalebMorin <cmorin2@my.centennialcollege.ca>
2019-03-15 02:27:46 -0400

    Added the UML diagrams

--- Group1_comp123_Assignment3/Tweets.cs
+++ Group1_comp123_Assignment3/Tweets.cs
@@ -8,8 +8,51 @@ namespace Group1_comp123_Assignment3
 {
     class Tweets
     {
-        static void Main(string[] args)
+        //So I think the point of this program is to make an object for every tweet in the file
+        //aderson sent us, save them in a json and read the json file back.
+        //easy peasy
+
+        private static int currentId = 1;
+
+        public int Id { get; set; }//Represents the unique identification of the tweet
+        public string From { get; set; }//represents the originator of the tweet
+        public string To { get; set; }//represents the intended recipient of the tweet
+        public string Message { get; set; }//represents the actual message in the tweet
+        public string Hashtag { get; set; }//represents the hashtags used in the tweet
+
+        public Tweet(string from, string to, string message, string hashtag)
         {
+
+        }
+        public override string ToString()
+        {
+
+        }
+        public static Tweet Parse(string line)
+        {
+            
+        }
+    }
+    class TweetManager
+    {
+        private static Tweet[] tweets;
+        private static string fileName;
+
+        static TweetManager()
+        {
+            
+        }
+        public static void ShowAll()
+        {
+
+        }
+        public static void ShowAll(string hashtag)
+        {
+
+        }
+        public static void ConvertToJson()
+        {
+
         }
     }
 }
