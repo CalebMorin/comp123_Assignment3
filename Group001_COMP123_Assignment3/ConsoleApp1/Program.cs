449e51edffa9dfe72a74bf421a0734276ad2644b
Corsaid <43212636+Corsaid@users.noreply.github.com>
2019-03-17 00:58:19 -0400

    Add files via upload

---
+++ Group001_COMP123_Assignment3/ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweet;
+using System.IO;
+using static System.Console;
+using System.Web.Script.Serialization;
+
+namespace Program
+{
+    class Program
+    {
+        //This is where we TEST our program
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class TweetManager
+    {
+        //I managed to get the Tweet[] array here without the red underline
+        private static Tweet.Tweet[] tweets;
+        private static string fileName;
+
+        static TweetManager()
+        {
+
+        }
+        //Displays all tweets
+        public static void ShowAll()
+        {
+
+        }
+        //Displays all the tweets with the same tag
+        public static void ShowAll(string hashtag)
+        {
+
+        }
+        //Serialize the array of tweets and saves them to a file called tweets.json
+        public static void ConvertToJson()
+        {
+
+        }
+    }
+}
---
+++ Group001_COMP123_Assignment3/Tweet/Tweet.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+/* Okay, so I was looking over your version and at the assignment, and I think he wants us to have the Tweet class and TweetManager class separated, where the TweetManager class
+ * is with the Program class. Hopefully I was able to put it in a way to make it easy for you to read. (Formatting is not my forte)
+ * So far, I've only reorganized the classes so that it fits the assignment requirements, I'm still working on the other parts. Ill push this for now though.
+ */
+namespace Tweet
+{
+    public class Tweet
+    {
+        private int currentID = 1;
+
+        public int Id { get; set; }//Tweet ID
+        public string From { get; set; }//Tweet Sender
+        public string To { get; set; }//Tweet Recipient
+        public string Message { get; set; }//Tweet Message
+        public string Hashtag { get; set; }//Tweet Tags
+
+        //Constructor that assigns the parameters to the properties
+        public Tweet(string from, string to, string message, string hashtag)
+        {            
+            From = from;
+            To = to;
+            Message = message;
+            Hashtag = hashtag;
+            Id = currentID;
+            currentID++;
+        }
+        //Overrides the original ToString() method to create our own
+        public override string ToString()
+        {
+            
+        }
+        //
+        public static Tweet Parse(string line)
+        {
+            Tweet[] tweets = new Tweet[29];
+
+           
+        }
+    }
+}
