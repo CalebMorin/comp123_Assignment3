f1732670d56d5adf8706d07aa545db37a47c7c84
Corsaid <43212636+Corsaid@users.noreply.github.com>
2019-03-19 19:58:54 -0400

    Add files via upload

---
+++ Group001_COMP123_Assignment3/Group001_COMP123_Assignment3/ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweeting;
+using System.IO;
+using static System.Console;
+using System.Web.Script.Serialization;
+
+namespace Program
+{
+    class Program
+    {
+        //This is where we TEST our program
+        static void Main(string[] args)
+        {
+            TweetManager.ShowAll();
+            ReadKey();
+        }
+    }
+    class TweetManager
+    {
+        //I managed to get the Tweet[] array here without the red underline
+        private static Tweet[] tweets;
+        private static string fileName;
+
+        static TweetManager()
+        {
+            FileStream fileStream = new FileStream("D:/Centennial/COMP123/Group001_COMP123_Assignment3/tweets.txt", FileMode.Open, FileAccess.Read);
+            StreamReader streamReader = new StreamReader(fileStream);           
+            tweets = new Tweet[29];
+            string line = streamReader.ReadLine();
+            while(line != null)
+            {
+                for(int i=0; i<tweets.Length; i++)
+                {
+                    tweets[i] = Tweet.Parse(line);
+                }
+                line = streamReader.ReadLine();
+            }
+        }
+        //Displays all tweets
+        public static void ShowAll()
+        {
+            fileName = @"tweets.txt";
+            foreach (string line in File.ReadAllLines("D:/Centennial/COMP123/Group001_COMP123_Assignment3/tweets.txt"))
+            {
+                WriteLine(line);
+            }
+        }
+        //Displays all the tweets with the same tag
+        public static void ShowAll(string hashtag)
+        {
+            fileName = @"tweets.txt";
+            foreach (string line in File.ReadAllLines("D:/Centennial/COMP123/Group001_COMP123_Assignment3/tweets.txt"))
+            {
+                if (line.Contains(hashtag))
+                    WriteLine(line);
+            }
+        }
+        //Serialize the array of tweets and saves them to a file called tweets.json
+        public static void ConvertToJson()
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            FileStream fileStream = new FileStream("tweets.json", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter streamWriter = new StreamWriter(fileStream);
+
+            Tweet[] allTweets = new Tweet[29];
+            //ETC ETC ETC
+        }
+    }
+}
---
+++ Group001_COMP123_Assignment3/Group001_COMP123_Assignment3/Tweet/Tweet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using System.Runtime;
+
+/* Okay, so I was looking over your version and at the assignment, and I think he wants us to have the Tweet class and TweetManager class separated, where the TweetManager class
+ * is with the Program class. Hopefully I was able to put it in a way to make it easy for you to read. (Formatting is not my forte)
+ * So far, I've only reorganized the classes so that it fits the assignment requirements, I'm still working on the other parts. Ill push this for now though.
+ */
+namespace Tweeting
+{
+    public class Tweet
+    {
+        private int currentID = 1;
+
+        public int Id { get; set; }//Tweet ID
+        public string From { get; set; }//Tweet Sender
+        public string To { get; set; }//Tweet Recipient
+        public string Message { get; set; }//Tweet Message
+        public string Hashtag { get; set; }//Tweet Tags
+
+        //Constructor that assigns the parameters to the properties
+        public Tweet(string from, string to, string message, string hashtag)
+        {            
+            From = from;
+            To = to;
+            Message = message;
+            Hashtag = hashtag;
+            Id = currentID;
+            currentID++;
+        }
+        //Overrides the original ToString() method to create our own
+        public override string ToString()
+        {
+            string messagePart = Message.Substring(0, Message.Length);
+            string tweetString = String.Format("{0,-2}{1,-8}{2,-8}{3,-8}");
+            return tweetString;
+        }
+        //
+        public static Tweet Parse(string line)
+        {
+            Tweet newTweet = new Tweet("", "", "", "");
+            string[] testTweet;
+
+            try
+            {
+                testTweet = line.Split(new char[] { '\t' });
+                newTweet = new Tweet(from: testTweet[0], to: testTweet[1], message: testTweet[2], hashtag: testTweet[3]);
+                return newTweet;
+                
+            }
+            catch (IndexOutOfRangeException)
+            {
+                newTweet.From = "Invalid";
+                newTweet.To = "Invalid";
+                newTweet.Message = "Invalid";
+                newTweet.Hashtag = "Invalid";
+            }
+            return newTweet;
+        }
+    }
+}
